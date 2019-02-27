18184860e6d034503533d3405250e012204dd510
Geeeva <ivana.gilich@gmail.com>
2019-02-27 16:56:18 +0100

    Updates

---
+++ precache-manifest.4963d9f71d00a35015a30d76fa16ed51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/Shopping-cart/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "72bf0389f4afe86427f5",
+    "url": "/Shopping-cart/static/js/runtime~main.72bf0389.js"
+  },
+  {
+    "revision": "fd98d903c82aae0bfd5b",
+    "url": "/Shopping-cart/static/js/main.fd98d903.chunk.js"
+  },
+  {
+    "revision": "a085a4ccf781bc12daba",
+    "url": "/Shopping-cart/static/js/2.a085a4cc.chunk.js"
+  },
+  {
+    "revision": "fd98d903c82aae0bfd5b",
+    "url": "/Shopping-cart/static/css/main.31ab1e67.chunk.css"
+  },
+  {
+    "revision": "a085a4ccf781bc12daba",
+    "url": "/Shopping-cart/static/css/2.c3f91a66.chunk.css"
+  },
+  {
+    "revision": "82917cb09ad9bd9378e17d20bc4069b4",
+    "url": "/Shopping-cart/index.html"
+  }
+];
